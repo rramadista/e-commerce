b5d6dfc491a55c2649d03da891f3204c7ea927af
Raden Ramadista <raden.ramadista@gmail.com>
2020-04-18 13:00:12 +0700

    Adding stripe and stripe checkout button into checkout page

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -4,9 +4,10 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import {
     selectCartItems,
-    selectCartTotal
+    selectCartTotal,
 } from '../../services/redux/cart/cart.selectors';
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
 const CheckoutPage = ({ cartItems, total }) => (
     <div className='checkout-page'>
@@ -27,18 +28,22 @@ const CheckoutPage = ({ cartItems, total }) => (
                 <span>Remove</span>
             </div>
         </div>
-        {cartItems.map(cartItem => (
+        {cartItems.map((cartItem) => (
             <CheckoutItem key={cartItem.id} cartItem={cartItem} />
         ))}
-        <div className='total'>
-            <span>TOTAL: ${total}</span>
+        <div className='total'>TOTAL: ${total}</div>
+        <div className='test-warning'>
+            *Please use the following test credit card for payments*
+            <br />
+            4242 4242 4242 4242 - Exp: 04/20 - CVV: 123
         </div>
+        <StripeCheckoutButton price={total} />
     </div>
 );
 
 const mapStateToProps = createStructuredSelector({
     cartItems: selectCartItems,
-    total: selectCartTotal
+    total: selectCartTotal,
 });
 
 export default connect(mapStateToProps)(CheckoutPage);
